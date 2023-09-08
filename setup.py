4cab60fcc5a6574a99667fa361de5673c896624b
fengminglin <1900258258@qq.com>
2023-09-08 16:12:51 +0100

    add new files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ggbond',
+    version='0.1',
+    author='FMlin',
+    author_email='sdulinfm@gmail.com',
+    description='medical image segmentation',
+    packages=find_packages(),
+)
